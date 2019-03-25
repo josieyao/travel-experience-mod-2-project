8c3e4e06739faff450712057bbf32e0b7a00b0f0
josieyao <yao.josephine@yahoo.com>
2019-03-25 15:35:44 -0500

    Changed schema for User and Receiver tables to have first name/last name. Changed schema for Itinerary table--took out Airport name/address and replaced it with Airport code

--- db/migrate/20190325191336_create_itineraries.rb
+++ db/migrate/20190325191336_create_itineraries.rb
@@ -1,15 +1,14 @@
 class CreateItineraries < ActiveRecord::Migration[5.2]
   def change
     create_table :itineraries do |t|
-      t.integer :flight_number
-      t.string :airline
-      t.string :seat_number
-      t.text :date
-      t.string :airport_name
-      t.string :airport_address
-      t.string :gate_number
       t.string :destination_name
       t.string :destination_address
+      t.string :airline
+      t.string :airport_code
+      t.integer :flight_number
+      t.string :gate_number
+      t.string :seat_number
+      t.text :date
       t.integer :user_id
       t.integer :receiver_id
 
